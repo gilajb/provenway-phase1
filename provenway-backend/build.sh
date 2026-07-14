75f0fe28db920935ef350463fa64e20706364f4a
gilajb <cjoybett@gmail.com>
2026-07-14 23:51:54 +0300

    Create or reset admin superuser on deploy

--- provenway-backend/build.sh
+++ provenway-backend/build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements/production.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "import os; from django.contrib.auth import get_user_model; U = get_user_model(); u = U.objects.get(email='cjoybett@gmail.com'); u.set_password(os.environ['TEMP_ADMIN_RESET']); u.save(); print('Password reset done')"
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; U = get_user_model(); u, created = U.objects.get_or_create(email='cjoybett@gmail.com', defaults={'is_staff': True, 'is_superuser': True}); u.is_staff = True; u.is_superuser = True; u.set_password(os.environ['TEMP_ADMIN_RESET']); u.save(); print('Created' if created else 'Updated')"
