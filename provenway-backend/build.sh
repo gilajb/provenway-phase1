09a00692f4396f04e72da97f24dfac5661951c69
gilajb <cjoybett@gmail.com>
2026-07-14 23:42:54 +0300

    Fix build.sh shell quoting for password reset

--- provenway-backend/build.sh
+++ provenway-backend/build.sh
@@ -4,10 +4,4 @@ set -o errexit
 pip install -r requirements/production.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-from django.contrib.auth import get_user_model
-U = get_user_model()
-u = U.objects.get(email='cjoybett@gmail.com')
-u.set_password(os.environ['TEMP_ADMIN_RESET'])
-u.save()
-print('Password reset done')
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; U = get_user_model(); u = U.objects.get(email='cjoybett@gmail.com'); u.set_password(os.environ['TEMP_ADMIN_RESET']); u.save(); print('Password reset done')"
